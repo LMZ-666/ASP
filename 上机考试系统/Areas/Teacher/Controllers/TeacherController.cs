438a8bf173b220c7d3f9d702a41e673c4a32e34e
明哲 廖 <326995871@qq.com>
2020-12-16 11:44:01 +0800

    修复考前页面中文显示“？？？”bug

--- 上机考试系统/Areas/Teacher/Controllers/TeacherController.cs
+++ 上机考试系统/Areas/Teacher/Controllers/TeacherController.cs
@@ -34,9 +34,13 @@ namespace 上机考试系统.Areas.Teacher.Controllers
         [HttpPost]
         public ActionResult BeforeTest(Exam exam)
         {
-            exam.Id = 3;
             exam.creator = "黄亚博";
             exam.creatorId = 1;
+            exam.has_cleaned = "否";
+            exam.has_saved = "否";
+            exam.has_stopped = "否";
+            exam.is_being = "否";
+            exam.test_upload = "试卷未上传";
             db.Exam.Add(exam);
             db.SaveChanges();
             return View(db.Exam.ToList());
