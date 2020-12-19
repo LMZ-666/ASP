00ca18363625907ce53fb5f2108f325377bc6973
User <User@LAPTOP-AFEO1N82>
2020-12-19 10:50:24 +0800

    考试界面前端修改

--- 上机考试系统/Areas/student/Controllers/StudentController.cs
+++ 上机考试系统/Areas/student/Controllers/StudentController.cs
@@ -7,17 +7,43 @@ using 上机考试系统.Models.DatabaseModel;
 
 namespace 上机考试系统.Areas.student.Controllers
 {
+    
     public class StudentController : Controller
     {
+        DatabaseEntities db = new DatabaseEntities();
+        static int MyTestId;
+        static String MyStudentName;
         // GET: student/Student
-        public ActionResult StudentIndex(String studentName)
+
+        public ActionResult InitStudent(String studentName,int TestId=0)
         {
-            ViewBag.studentName = studentName;
+            
+            MyTestId = TestId;
+            MyStudentName = studentName;
+            return RedirectToAction("StudentIndex");
+        }
+        public ActionResult StudentIndex()
+        {
+
+            ViewBag.studentName = MyStudentName;
             return View();
         }
-        public ActionResult StartTest(Exam exam)
+        public ActionResult StartTest()
         {
-            return View();
+
+            Exam EX = db.Exam.Find(MyTestId);
+
+            String str;
+            if(EX!=null)
+            {
+                str = String.Format("本场考试的科目名称为：{0}，考试时间为：{1}，监考教师是：{2}", EX.name, EX.time, EX.creator);
+            }
+            else
+            {
+                str = "没有要进行的考试";
+            }
+            ViewBag.Message = str;
+            return View(EX);
         }
     }
 }
